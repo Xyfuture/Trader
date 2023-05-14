7749c5e208c1d5b7268aa7d671d84a6ecabea60e
Xyfuture <49277529+Xyfuture@users.noreply.github.com>
2023-05-15 00:41:46 +0800

    temp change

--- trader/Order.py
+++ trader/Order.py
@@ -1,11 +1,21 @@
+import asyncio
 
 
 class Order:
-    def __init__(self,direction=0,instrument=None,order_type=None,size=None,**kwargs):
+    def __init__(self, direction=0, proportion=0.2, instrument=None, order_type=None, order_source='strategy'
+                 , size=None,
+                 take_profit_price=0, stop_loss_price=0, **kwargs):
         self.instrument = instrument
         self.direction = direction
+        self.proportion = proportion
         self.order_type = order_type
         self.size = size
+
+        self.take_profit_price = take_profit_price
+        self.stop_loss_price = stop_loss_price
+
+        self.place_status = "unplaced" # placed, failed, successful,trying,
+        self.order_source = order_source  # strategy take_profit stop_loss
 
         self.kwargs = kwargs
 
@@ -17,5 +27,16 @@ class Order:
         print(self)
 
 
+class AsyncOrderTask:
+    def __init__(self, order):
+        self.order = order
+        self.event = asyncio.Event()
+        self.ret = False
 
+    async def wait(self)->bool:
+        await self.event.wait()
+        return self.ret
 
+    def set(self,ret:bool):
+        self.ret = ret
+        self.event.set()
--- trader/OrderManager.py
+++ trader/OrderManager.py
@@ -1,5 +1,7 @@
 from trader.Broker import Broker
-from trader.Order import Order
+from trader.Order import Order, AsyncOrderTask
+import queue
+import asyncio
 
 
 class OrderManager:
@@ -9,48 +11,88 @@ class OrderManager:
         self.instrument = config['instrument']
         self.risk_strategy = config['risk_strategy']
 
-    def place_order(self, order: Order):
-        order = self.complete_order(order)
+        self.all_orders = []
+        self.pending_orders = asyncio.Queue()  # wait for trade
+        self.under_risk_orders = []  # unsold buy orders
+
+    async def process_order(self,order:Order):
+        pass
+
+    async def place_order(self, order: Order) -> bool:
+        order_task = AsyncOrderTask(order)
+        await self.pending_orders.put(order_task)
+        ret = await order_task.wait()
+        return ret
+
+    async def _place_order(self, order: Order) -> bool:
+        order = self.fill_order(order)
 
         if self.check_order(order):
             self.broker.place_order(order)
             print(f"{order}\nhas been placed to exchange")
         else:
-            print(f"{order}\n passed by bot")
+            print(f"{order}\npassed by bot")
+
+        return True
 
     async def order_risk_control(self):
-        pass
+        # loop check order risk
+        while True:
+            if self.under_risk_orders:
+                last_price = self.broker.fetchTicker(self.instrument)['last']
+                risk_orders = [order for order in self.under_risk_orders if
+                               order.stop_loss_price > last_price or order.take_profit_price < last_price]
 
-    def size_order(self, order) -> Order:
-        # 选中策略
-        # 计算容量
-        # allin 发射
-        if order.direction == 1:
-            # long trade
-            currency = order.instrument.split('-')[1]
-            balance = self.broker.get_balance(currency)
-            last_price = self.broker.fetchTicker(self.instrument)['last']
-            amount = (balance / last_price) * 0.99
-            if self.risk_strategy == 'allin':
-                order.size = amount
-            else:
-                raise "Not implement"
-        elif order.direction == -1:
-            currency = order.instrument.split('-')[0]
-            balance = self.broker.get_balance(currency)
-            if self.risk_strategy == 'allin':
-                order.size = balance
-            else:
-                raise "Not implement"
+                for order in risk_orders:
+                    source = "take_profit" if order.take_profit_price > last_price else "stop_loss"
+                    new_order = Order(direction=-1, size=order.size, order_source=source)
+                    ret = await self.place_order(new_order)
 
-        return order
+                    if ret:
+                        self.under_risk_orders.remove(order)
 
-    def complete_order(self, order) -> Order:
+            await asyncio.sleep(60)
+
+    def fill_order(self, order) -> Order:
+        def strategy_size_order(order) -> Order:
+            # 选中策略
+            # 计算容量
+            # allin 发射
+            if order.direction == 1:
+                # long trade
+                currency = order.instrument.split('-')[1]
+                balance = self.broker.get_balance(currency)
+                buy_balance = balance * order.proportion
+                if 0 < buy_balance < balance:
+                    last_price = self.broker.fetchTicker(self.instrument)['last']
+                    amount = (buy_balance / last_price)
+                    order.size = amount
+                else:
+                    order.size = 0
+
+            elif order.direction == -1:
+                if not self.under_risk_orders.empty():
+                    cur_order = self.under_risk_orders.get()
+                    order.size = cur_order.size
+                else:
+                    order.size = 0
+
+            return order
+
+        # basic info
         order.instrument = self.instrument
-
         if not order.order_type:
             order.order_type = 'market'
-        order = self.size_order(order)
+
+        if order.order_source == 'strategy':
+            order = strategy_size_order(order)
+        elif order.order_source == 'take_profit':
+            pass
+        elif order.order_source == 'stop_loss':
+            pass
+        else:
+            raise "not implement"
+
         return order
 
     def check_order(self, order) -> bool:
